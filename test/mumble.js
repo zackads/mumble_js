4dbb286417e339ee08b54e930a0470b892d0ef06
Shaw Malcolm <shawmalcolm@protonmail.com>
2020-10-14 10:55:17 +0100

    new test with complex constant

--- test/mumble.js
+++ test/mumble.js
@@ -5,11 +5,17 @@ import assert from 'assert';
 describe("Mumble", function () {
   it("handles empty strings", function () {
     const input_string = "";
-    assert.strictEqual("", mumble(input_string));
+    assert.strictEqual('', mumble(input_string));
   });
-
+  
   it("handles single character inputs", function () {
     const input_string = "Z";
     assert.strictEqual("Z", mumble(input_string));
   });
+
+  it("handles multiple character inputs", function () {
+    const input_string = "YTREWQ";
+    assert.strictEqual("YTREWQ", mumble(input_string));
+  });
 });
+
